030f9c6e13729a1cf90e9ea910f28c5aa99b4ff5
Nicolas Constantin <nicolasconstantin@hotmail.fr>
2020-11-12 14:36:32 +0100

    models and ingredient seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# require 'json'
+# require 'open-uri'
+
+# Ingredient.destroy_all if Rails.env.development?
+puts "start"
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+Ingredient.destroy_all if Rails.env.development?
+
+# Actually fetch the contents of the remote URL as a String.
+buffer = open(url).read
+
+ingredients = JSON.parse(buffer)
+
+ingredients['drinks'].each do |ingredient|
+  # Ingredient.create!(name: ingredient["strIngredient1"])
+  puts ingredient["strIngredient1"]
+end
